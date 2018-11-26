35cf512de6b00a3fa3c74735cb738a3163755b8a
flocurity <30314113+flocurity@users.noreply.github.com>
2018-11-26 15:44:50 +0100

    add some help

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ This dockerfile builds the image needed to build a wiki from
 + MarkdownPP
 + some personal tricks to repare broken links from templates
 + MkDocs
+
+## Usage
+``` properties
+-m : mode. Supported => destruct_mode => delete mdpp files before publishing. For CI only !
+-l : use linkchecker to check for broken links
+-v : set mkdocs to verbose mode - for debugging only
+-u : an unicorn in this crual world shouldn't hurt anyone
+```
--- files/transform.sh
+++ files/transform.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-while getopts m:vlu option; do
+
+# Function: Print a help message.
+usage() {
+  echo "Usage: $0 [ -m MODE ] [ -v ] [ -l ] [ -u ]" 1>&2
+  echo "[ -m MODE ] : supported mode => destruct_mode => delete mdpp files before publishing. For CI only " 1>&2
+  echo "[ -v ] : set mkdocs to verbose mode - for debugging only" 1>&2
+  echo "[ -l ] : use linkchecker to check for broken links" 1>&2
+  echo "[ -u ] : see for yourself !" 1>&2
+}
+
+# Function: Exit with error.
+exit_abnormal() {
+  usage
+  exit 1
+}
+
+while getopts m:vluh option; do
   case "${option}" in
     m)
       MODE=${OPTARG}
@@ -14,6 +30,15 @@ while getopts m:vlu option; do
       ;;
     u)
       cat /home/md_user/hidden_file
+      ;;
+    :)
+      echo "Error: -${OPTARG} requires an argument."
+      usage
+      exit 42
+      ;;
+    *)
+      usage
+      exit 42
       ;;
   esac
 done
